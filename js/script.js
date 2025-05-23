c8a6012528162c3f91cba0dc5e9cfaba0ac6d8e7
exskandr <exskandr@gmail.com>
2025-05-23 12:02:24 +0300

    add css, js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Тут можна додати JavaScript для інтерактивних елементів,
+// наприклад, для анімацій, валідації форм (фронтенд)
+// або динамічного завантаження контенту.
+// Поки що файл порожній.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Сайт завантажено!');
+    // Приклад: можна додати функціонал для кнопки "Дізнатись більше"
+    // const learnMoreBtn = document.querySelector('.btn');
+    // if (learnMoreBtn) {
+    //     learnMoreBtn.addEventListener('click', () => {
+    //         alert('Переходимо на сторінку "Про мене"!');
+    //     });
+    // }
+});
